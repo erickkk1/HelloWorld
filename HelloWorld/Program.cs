255d2bb368da33e19fc4d7430ac57684452d8f0f
Eric Kouakou <erickouakou@usf.edu>
2019-01-15 17:06:48 -0500

    version 2

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,11 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            // version 2!
+        
+            Console.WriteLine("Press any key to conmtinue...");
             Console.WriteLine("Hello World!");
+            Console.ReadKey(true);
         }
     }
 }
